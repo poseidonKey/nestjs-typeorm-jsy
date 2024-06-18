7a2186a5ed95a23c8f7be3792ff1a7e7983de4f1
poseidon <jsy10303@gmail.com>
2024-06-18 14:59:02 +0900

    섹션10 컬럼 프로퍼티 정리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { UserModel } from './entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -11,4 +11,30 @@ export class AppController {
     @InjectRepository(UserModel)
     private readonly userRepository: Repository<UserModel>,
   ) {}
+
+  @Get('users')
+  async getUser() {
+    return await this.userRepository.find({
+      // select: {
+      //   id: true,
+      //   title: true,
+      // },
+    });
+  }
+  @Post('users')
+  async postUser() {
+    return await this.userRepository.save({
+      title: 'test title',
+    });
+  }
+  @Patch('users/:id')
+  async patchUser(@Param('id') id: string) {
+    const user = await this.userRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+    return await this.userRepository.save({
+      ...user,
+      title: user.title + '0',
+    });
+  }
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -10,17 +10,43 @@ import {
 
 @Entity()
 export class UserModel {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    // 데이터 베이스에서 인지하는 컬럼 타입
+    // 자동으로 유추 됨
+    // 프로퍼티의 상황에 따라 오류 발생 되기도 함
+    type: 'varchar',
+    // 데이터 베이스 칼럼 이름
+    // 프로퍼티 이름으로 자동 유추됨
+    name: 'title',
+    // 값의 길이, 입력할 수 있는 글자의 길이 지정
+    length: 300,
+    nullable: true,
+    // true 이면 처음 저장할 때만 값 지정 가능
+    // 이후에는 갑 변경 불가능
+    update: true,
+    // 기본 값이 true 즉 title을 가져와라
+    // controller 에서 개별 조정 가능
+    select: true,
+    // 아무 값도 입력 안 했을 때 기본 값이 필요한 경우
+    default: 'default valeu',
+    // 컬럼 중에서 유일한 값이 돼야하는 가 여부, 기본 값 false, email 등에 사용
+    // null도 중복되면 안됨.
+    unique: false,
+  })
   title: string;
+
   @CreateDateColumn()
   createdAt: Date;
+
   @UpdateDateColumn()
   updatedAt: Date;
+
   @VersionColumn()
   version: number;
+
   @Column()
   @Generated('uuid')
   additionalId: string;
